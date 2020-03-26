12455b272ce54b161c0e3eaf311efc91ecb8cb29
isakal <sakyYT@gmail.com>
2020-03-26 23:31:58 +0100

    Refine .eslintrc.js and add some more rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+const OFF = 0, WARN = 1, ERROR = 2;
 module.exports = {
   "env": {
     "es6": true,
@@ -14,6 +15,21 @@ module.exports = {
     "ecmaVersion": 2018
   },
   "rules": {
+    "no-extra-parens": ERROR,
+    "semi": [ERROR, "never"],
+    "block-scoped-var": WARN,
+    "consistent-return": WARN,
+    "curly": ERROR,
+    "eqeqeq": [ERROR, "smart"],
+    "max-len": [WARN, 100],
+    "max-params": [WARN, 8],
+    "max-depth": [WARN, 6],
+    "max-nested-callbacks": [WARN, 6],
+    "spaced-comment": [ERROR, "always"],
+    "no-mixed-spaces-and-tabs": WARN,
+    "no-lonely-if": WARN,
+    "no-trailing-spaces": WARN,
+
 
   }
 }

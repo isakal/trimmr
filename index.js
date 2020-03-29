759cdc08ed053d6a48bc35c8784910ea3bb7ca8c
isakal <sakyYT@gmail.com>
2020-03-29 14:03:20 +0200

    Fix response bodies and add .env.sample for easier project configuration

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
 
 const Url = require('./models/url')
 
 const app = express()
-
-app.use(bodyParser.json())
-
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
+
+app.use(express.json())
 
 // try connectinng to mongo database
 mongoose.connect(process.env.DB_URI, options).then(() => {
@@ -20,20 +18,26 @@ mongoose.connect(process.env.DB_URI, options).then(() => {
 app.get('/all-urls', async (req, res) => {
   // query all urls and send them
   const urls = await Url.find({})
-  res.send(urls)
+  res.json({
+    urls: urls
+  })
 })
 
 app.post('/add-url', async (req, res) => {
   // create url with req.body.full, save it and return shortened url back to client
   const urlAlreadyExists = await Url.findOne({ full: req.body.full })
   if (urlAlreadyExists) {
-    return res.send(urlAlreadyExists.short)
+    return res.json({
+      url: urlAlreadyExists.short
+    })
   }
   // create new url if it doesn't already exists
   const url = new Url({ full: req.body.full })
   await url.save()
 
-  res.send(url.short)
+  res.json({
+    url: url.short
+  })
 })
 
 app.get('/:shortUrl', async (req, res) => {
@@ -41,9 +45,12 @@ app.get('/:shortUrl', async (req, res) => {
   const url = await Url.findOne({ short: req.params.shortUrl })
   // if url doesn't exists, send a 404
   if (url === null) {
-    res.sendStatus(404)
+    res.status(404).json({
+      error: 'No site found.'
+    })
   }
   // raise clicks by one and redirect the user to the full url
+  // FIXME: implement transactions
   url.clicks++
   await url.save()
 
